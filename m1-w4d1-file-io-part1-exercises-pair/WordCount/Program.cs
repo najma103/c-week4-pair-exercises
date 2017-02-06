570fa6647236de56ee41d9d79291d761ddf6b1b7
Najma Aden <najma.aden@gmail.com>
2017-02-06 15:53:42 -0500

    Andrew and Najma's pair exercise

--- m1-w4d1-file-io-part1-exercises-pair/WordCount/Program.cs
+++ m1-w4d1-file-io-part1-exercises-pair/WordCount/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace WordCount
 {
@@ -10,6 +12,24 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the filepath ?");
+            string filePath = Console.ReadLine();
+            //filePath = "@" + filePath;
+            //filePath = @"C:\WordSearch";
+            string filename = @"alices_adventures_in_wonderland.txt";
+
+            // Generating a full path from a folder and a file name
+            string fullPath = Path.Combine(filePath, filename);
+
+            bool directoryExists = Directory.Exists(filePath);
+
+            if (directoryExists)
+            {
+                ReadAndProcessWord.ReadACharacterFile (fullPath);
+                ReadAndProcessWord.printTotalCount();
+            }
+
+
         }
     }
 }
